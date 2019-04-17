bfc069925c3ff289e4b1d5226f083ffe5731c509
Michael Bahari <bahayana@gmail.com>
2019-04-17 14:55:29 +1000

    added controllers and methods, plus views for listings

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,30 @@
+class ListingsController < ApplicationController
+    def create
+        #create new listing
+    end
+
+    def update
+        #updates current listing
+    end
+
+    def index
+        #shows all listings
+    end
+
+    def edit
+        #show the edit form 
+    end
+
+    def destroy
+        #deletes current listing
+    end
+
+    def new
+        #shows form for creating a new listing
+    end
+
+    def show
+        #
+    end
+    
+end
