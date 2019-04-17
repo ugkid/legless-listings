5f7beda7d4c983a42c894b6df9feeeda6b38def2
Michael Bahari <bahayana@gmail.com>
2019-04-17 12:52:08 +1000

    Created breed, listing models and migrations

---
+++ db/migrate/20190417024949_create_listings.rb
@@ -0,0 +1,17 @@
+class CreateListings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.text :description
+      t.references :breed, foreign_key: true
+      t.integer :sex
+      t.integer :price
+      t.integer :deposit
+      t.string :date_of_birth
+      t.string :date
+      t.string :diet
+
+      t.timestamps
+    end
+  end
+end
