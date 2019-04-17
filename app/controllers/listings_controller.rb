3cfb1ba08771271bd9e3486090517737ca43364c
Michael Bahari <bahayana@gmail.com>
2019-04-17 16:15:48 +1000

    added in breed and sex fields

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -26,6 +26,8 @@ class ListingsController < ApplicationController
     def new
         #shows form for creating a new listing
         @listing = Listing.new
+        @breeds = Breed.all
+        @sexes = Listing.sexes.keys
     end
 
     def show
